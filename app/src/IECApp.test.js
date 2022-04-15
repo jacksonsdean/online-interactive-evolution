0fac681fc40e24150201c778ffb4ba29d4143c96
Jackson Dean <Jackson.Dean@uvm.edu>
2022-04-15 13:14:02 -0400

    add tests for app

---
+++ app/src/IECApp.test.js
@@ -0,0 +1,29 @@
+// IECApp.test.js
+
+import React from "react";
+import { unmountComponentAtNode } from "react-dom";
+import { render, screen } from '@testing-library/react';
+
+import { act } from "react-dom/test-utils";
+import IECApp  from "./IECApp";
+let container = null;
+
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+
+test('renders learn react link', () => {
+  render(<IECApp />);
+  const linkElement = screen.getByText(/Welcome/i);
+  expect(linkElement).toBeInTheDocument();
+});
