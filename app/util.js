418ca5eac01a24f7cb41e43385e7d8911f7020ce
Jackson Dean <Jackson.Dean@uvm.edu>
2022-04-15 13:39:35 -0400

    remove old file

--- app/util.js
+++
@@ -1,9 +0,0 @@
-
-function testRestAPI() {
-    fetch('https://5c9gzxp0r0.execute-api.us-east-1.amazonaws.com/test/helloworld?name=John&city=Seattle')
-    .then(response => response.json())
-    .then(data => console.log(data));
-
-}
-
-export { testRestAPI };
