8c14a5e0857ed1c605afaf8360e9be0997052d28
Jackson Dean <Jackson.Dean@uvm.edu>
2022-04-17 16:15:32 -0400

    add tests for populationGrid

---
+++ app/src/components/populationGrid.test.js
@@ -0,0 +1,28 @@
+// populationGrid.test.js
+
+import React from "react";
+import { unmountComponentAtNode } from "react-dom";
+import { render, screen } from '@testing-library/react';
+
+import PopulationGrid from "./populationGrid";
+let container = null;
+
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+
+test('renders population grid', () => {
+  render(<PopulationGrid />);
+  const linkElement = screen.getByText(/Please wait../i);
+  expect(linkElement).toBeInTheDocument();
+});
