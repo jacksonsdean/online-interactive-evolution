9d927c1d733e90d494bccdbe1ff9b0f0d13cfc6d
Jackson Dean <jackson.dean@uvm.edu>
2022-04-21 16:39:09 -0400

    change to post request

--- frontend/src/components/populationGrid.js
+++ frontend/src/components/populationGrid.js
@@ -8,6 +8,10 @@ function to_ids(a){
     return b;
 }
 
+function post(url, data) {
+    return fetch(url, {method: "POST", headers: {'Content-Type': 'application/json'}, body: JSON.stringify(data)});
+}
+
 class IndividualButton extends React.Component {
     render() {
         return (
@@ -28,7 +32,7 @@ class PopulationGrid extends React.Component {
 
     componentDidMount() {
         const apiUrl = 'https://5c9gzxp0r0.execute-api.us-east-1.amazonaws.com/test/interactive-evolutionary-computation?ids=1,2,3,4,5,10';
-        fetch(apiUrl)
+        post(apiUrl, {"TEST_POST": "TEST_POST"})
             .then((response) => response.json())
             .then((data) => this.setState({ population_ids: to_ids(data) }));
     }
