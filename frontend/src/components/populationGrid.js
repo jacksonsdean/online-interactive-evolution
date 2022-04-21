f7321704dbadaa0a9d453524f9de64e5275efea1
Jackson Dean <jackson.dean@uvm.edu>
2022-04-21 17:21:33 -0400

    fix typo

--- frontend/src/components/populationGrid.js
+++ frontend/src/components/populationGrid.js
@@ -21,8 +21,8 @@ class PopulationGrid extends React.Component {
     }
 
     componentDidMount() {
-
-        const apiUrl = API_URL + "/?ids="+ids_to_string([1, 2, 3]);
+        // interactive-evolutionary-computation?ids=1,2,3,4,5,10
+        const apiUrl = API_URL + "?ids="+ids_to_string([1, 2, 3]);
         post(apiUrl, {"TEST_POST": "TEST_POST"})
             .then((response) => response.json())
             .then((data) => this.setState({ population_ids: string_to_ids(data) }));
