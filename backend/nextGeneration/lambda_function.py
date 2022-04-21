91c1f835bdfd22123a1e69a686bbe1daff64b7e5
Jackson Dean <jackson.dean@uvm.edu>
2022-04-21 17:25:00 -0400

    add error handling to lambda function

--- backend/nextGeneration/lambda_function.py
+++ backend/nextGeneration/lambda_function.py
@@ -3,14 +3,26 @@ import json
 
 def lambda_handler(event, context):
     """Handle an incoming request from Next Generation."""
-    ids = event["queryStringParameters"]['ids']
-    print(context)
-    return {
-            'statusCode': 200,
-            'headers': {
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-            },
-            'body': json.dumps(str(ids))
-        }
+    try:
+        ids = event["queryStringParameters"]['ids']
+        print(context)
+        return {
+                'statusCode': 200,
+                'headers': {
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+                },
+                'body': json.dumps(str(ids))
+            }
+    except Exception as e:
+        print(e)
+        return {
+                'statusCode': 500,
+                'headers': {
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+                },
+                'body': json.dumps(str(e))
+            }
