4abb65ac7f88ec46a8cdae7c3c78f94e98b127a0
Jackson Dean <jackson.dean@UVM.EDU>
2022-04-22 14:23:00 -0400

    improve testing process

--- backend/nextGeneration/interactive_testing.py
+++ backend/nextGeneration/interactive_testing.py
@@ -9,5 +9,7 @@ config = Config()
 cppn = CPPN(config)
 image_data = cppn.get_image_data_fast_method(32,32,"L")
 print(np.min(image_data), np.max(image_data))
+for n in cppn.node_genome:
+    print(n.activation)
 plt.imshow(image_data, cmap='gray', vmin = -1, vmax = 1)
 plt.show()
--- backend/nextGeneration/network_util.py
+++ backend/nextGeneration/network_util.py
@@ -16,11 +16,7 @@ def name_to_fn(name)->callable:
     return fns[[f[0] for f in fns].index(name)][1]
 
 
-def choose_random_function()->callable:
+def choose_random_function(config)->callable:
     """Chooses a random activation function from the activation function module."""
-    available_fns = inspect.getmembers(sys.modules["activation_functions"])
-    fns = [f for f in available_fns if len(f) > 1 and\
-        isinstance(f[1], type(choose_random_function))]
-    random_fn = random.choice(fns)
-    callable_function= name_to_fn(random_fn[0])
-    return  callable_function
+    random_fn = random.choice(config.activations)
+    return  random_fn
