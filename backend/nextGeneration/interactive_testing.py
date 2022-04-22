1827067bb1680598b9383e17e4dff6cd3cd8e654
Jackson Dean <jackson.dean@UVM.EDU>
2022-04-22 14:28:21 -0400

    add testing for color images

--- backend/nextGeneration/interactive_testing.py
+++ backend/nextGeneration/interactive_testing.py
@@ -7,9 +7,14 @@ from cppn import CPPN
 #%%
 config = Config()
 cppn = CPPN(config)
-image_data = cppn.get_image_data_fast_method(32,32,"L")
+image_data = cppn.get_image_data_fast_method(32,32)
 print(np.min(image_data), np.max(image_data))
-for n in cppn.node_genome:
-    print(n.activation)
+
 plt.imshow(image_data, cmap='gray', vmin = -1, vmax = 1)
+plt.show()
+config.color_mode = "RGB"
+cppn_color = CPPN(config)
+image_data = cppn_color.get_image_data_fast_method(32,32)
+print(image_data.shape)
+plt.imshow(image_data, vmin = -1, vmax = 1)
 plt.show()
