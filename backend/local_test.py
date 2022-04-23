7ed09a0286ee0fb19d17976500355c5e6c2b62e4
Jackson Dean <jackson.dean@uvm.edu>
2022-04-23 17:42:08 -0400

    allow local testing of lambda function within docker

--- backend/local_test.py
+++ backend/local_test.py
@@ -1,8 +1,13 @@
 from nextGeneration.lambda_function import lambda_handler
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/test', methods=['POST'])
+@app.route('/', methods=['POST'])
 def test():
+    """Handle an incoming request from Next Generation."""
+    print(request.data)
     return lambda_handler({"queryStringParameters": {"ids": "1,2,3"}}, None)
+
+if __name__ == "__main__":
+    app.run()
