a86992228ad7f018b55c4c1f1fb3b144d41ddc33
Jackson Dean <jackson.dean@uvm.edu>
2022-04-23 19:43:06 -0400

    reduce code complexity

--- backend/local_test.py
+++ backend/local_test.py
@@ -1,12 +1,14 @@
 from nextGeneration.lambda_function import lambda_handler
 from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) # Allow cross-origin requests
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
 def test():
     """Handle an incoming request from Next Generation."""
-    print(request.data)
+    print("request:", request)
     return lambda_handler({"queryStringParameters": {"ids": "1,2,3"}}, None)
 
 if __name__ == "__main__":
