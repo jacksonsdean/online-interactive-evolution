9c9151aadd12afbe9cc1d53668bb3c735b1e1c36
Jackson Dean <jackson.dean@uvm.edu>
2022-04-26 18:18:57 -0400

    clean up

--- backend/local_test.py
+++ backend/local_test.py
@@ -17,16 +17,12 @@ server = LocalServer(__name__)
 @server.route('/', methods=['POST', 'GET'])
 def test():
     """Handle an incoming request from Next Generation."""
-    print("request:", request)
     return lambda_handler(request.get_json(), None)
 
 def run_server():
     """Create and run a local server for testing"""
     global server
     server.run()
-
-
-
 
 if __name__ == "__main__":
     run_server()
