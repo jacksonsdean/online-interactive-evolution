67076962d35fb76992cf4016cfb693d74bbb3c8b
Jackson Dean <jackson.dean@uvm.edu>
2022-04-28 19:26:12 -0400

    update default config

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,5 +1,5 @@
 export const API_URL = process.env.REACT_APP_NODE_ENV === "local" ? "http://127.1.1.1:5000" : (process.env.REACT_APP_NODE_ENV === "production" ? "https://5c9gzxp0r0.execute-api.us-east-1.amazonaws.com/prod/next-gen" : "https://vln9z6a7gi.execute-api.us-east-1.amazonaws.com/development/next-gen")
 
 export const INITIAL_REQUEST = {
-    "operation": "reset", "config": { "population_size": 10, "activations": ["identity"], "output_activation": "tanh" },
+    "operation": "reset", "config": { "color_mode": "RGB", "res_w":64,"res_h":64, "population_size": 18, "activations": ["identity", "gauss", "sin", "cos", "sawtooth", "tanh", "sigmoid"], "output_activation": "tanh" },
 }
