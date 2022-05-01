2610f6726b0c9dbaa6c9e8628dc3ab680137dfaf
Jackson Dean <jackson.dean@uvm.edu>
2022-05-01 16:51:33 -0400

    redesign frontend

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,6 +1,12 @@
 export const API_URL = process.env.REACT_APP_NODE_ENV === "local" ? "http://127.1.1.1:5000" : (process.env.REACT_APP_NODE_ENV === "production" ? "https://5c9gzxp0r0.execute-api.us-east-1.amazonaws.com/prod/next-gen" : "https://vln9z6a7gi.execute-api.us-east-1.amazonaws.com/development/next-gen")
 
-export const DEFAULT_CONFIG = { "color_mode": "RGB", "res_w":64,"res_h":64, "population_size": 18, "activations": ["identity", "gauss", "sin", "cos", "sawtooth", "tanh", "sigmoid"], "output_activation": "tanh" }
+export const DEFAULT_CONFIG = { 
+    "color_mode": "RGB",
+    "res_w": 128,
+    "res_h": 128,
+    "population_size": 9,
+    "activations": ["identity", "gauss", "sin", "cos", "sawtooth", "tanh", "sigmoid"],
+    "output_activation": "" }
 
 export const RESET_OPERATION = "reset"
 export const NEXT_GEN_OPERATION = "next_gen"
--- frontend/src/components/populationGrid.js
+++ frontend/src/components/populationGrid.js
@@ -25,18 +25,38 @@ class IndividualButton extends React.Component {
         const url = "data:image/png;base64," + parsed.join("")
         const selectionStyle = individual.selected ? styles.selected : styles.unselected
         return (
-            <button className={styles.individualButton + " " + selectionStyle} onClick={this.clicked}>
+            <button className={styles.individualButton + " " + selectionStyle} onClick={this.clicked} style={this.props.style}>
                 <img className={styles.individualImg} src={url} alt={individual.name} />
             </button>
         )
     }
 }
 
+class LoadingSpinner extends React.Component {
+    render() {
+        return (
+            <div className="spinner-container">
+              <div className={styles.loadingSpinner} data-testid="spinner"/>
+            </div>
+          );
+    }
+}
+
 class NextGenerationButton extends React.Component {
     render() {
         return (
-            <button className={styles.nextGenButton + " " + (this.props.loading ? styles.loading:"")} onClick={this.props.onClick} disabled={this.props.loading}>
-                {this.props.loading?"Loading...": "Next Generation"}
+            <button style={this.props.style} className={styles.nextGenButton + " " + (this.props.loading ? styles.loading:"")} onClick={this.props.onClick} disabled={this.props.loading}>
+                {this.props.loading?"Loading...": "Next Generation \u21E8"}
+            </button>
+        )
+    }
+}
+
+class PreviousGenerationButton extends React.Component {
+    render() {
+        return (
+            <button style={this.props.style} className={styles.nextGenButton + " " + (this.props.loading ? styles.loading:"")} onClick={this.props.onClick} disabled={this.props.loading}>
+                {this.props.loading?"Loading...": "Previous Generation \u21E6"}
             </button>
         )
     }
@@ -47,6 +67,11 @@ class PopulationGrid extends Grid {
     constructor(props) {
         super(props);
         this.state = { population: [], loading: true };
+        this.history = [];
+        this.nextGenerationClicked = this.nextGenerationClicked.bind(this);
+        this.previousGenerationClicked= this.previousGenerationClicked.bind(this);
+        this.config = DEFAULT_CONFIG;
+        this.config.seed = Math.round(Math.random()*10000);
     }
 
     handleNewData(data){
@@ -79,10 +104,34 @@ class PopulationGrid extends Grid {
         this.setState({ population: pop, loading: false });
     }
 
+    nextGenerationClicked(){
+        this.history.push(this.state.population)
+        this.setState({loading:true });
+        nextGeneration(this.state.population, this.config).then((data) => {
+            this.handleNewData(data)
+        }).catch((err) => {
+            console.log(err)
+            this.setState({ population: this.state.population, loading: false });
+        })
+    }
+
+    previousGenerationClicked(){
+        if (this.history.length === 0) {
+            return
+        }
+        this.setState({loading:true });
+        nextGeneration(this.history.pop(), this.config).then((data) => {
+            this.handleNewData(data)
+        }).catch((err) => {
+            console.log(err)
+            this.setState({ loading: false });
+        })
+    }
+
     componentDidMount() {
-        initialPopulation(DEFAULT_CONFIG)
+        initialPopulation(this.config)
             .then((data) => {
-               this.handleNewData(data)
+                this.handleNewData(data)
             }).catch((err) => {
                 console.log(err)
             })
@@ -90,23 +139,21 @@ class PopulationGrid extends Grid {
 
     render() {
         if (typeof (this.state.population) === 'undefined' || this.state.population.length === 0) {
-            return (<p>Please wait...</p>)
+            return <LoadingSpinner/>
         }
+        const gridWidth = this.config.res_w * (1+Math.sqrt(this.config.population_size));
+        const individualWidth = (100/(1+Math.sqrt(this.config.population_size))).toString() + "%";
         // a grid of the population's individuals' images as buttons
-        return (<><div className={styles.populationGrid}>
-            <Grid >
-                {this.state.population.map((obj, index) => <IndividualButton key={index} individual={obj}></IndividualButton>)}
+        return (<><div className={styles.populationGrid} style={{width: gridWidth, maxWidth:"95vw", maxHeight:"60%"}}>
+            <Grid row={true} expanded={true} justify="center">
+                    {this.state.population.map(
+                        (obj, index) => <IndividualButton style={{width:individualWidth, maxHeight:"30vh",maxWidth:"30vh"}}key={index} individual={obj}></IndividualButton>)}
             </Grid>
         </div>
-            <NextGenerationButton loading={this.state.loading} onClick={() => {
-                this.setState({ population: this.state.population, loading:true });
-                nextGeneration(this.state.population, DEFAULT_CONFIG).then((data) => {
-                    this.handleNewData(data)
-                }).catch((err) => {
-                    console.log(err)
-                    this.setState({ population: this.state.population, loading: false });
-                })
-            }}></NextGenerationButton>
+            {this.state.loading ? <LoadingSpinner/> :<>
+                <NextGenerationButton style={{width: gridWidth/2}} loading={this.state.loading} onClick={this.nextGenerationClicked}/>
+                <PreviousGenerationButton style={{width: gridWidth/2}} loading={this.state.loading} onClick={this.previousGenerationClicked}/>
+            </>}
         </>
         );
     }
